da153a267ff742ae24dcf318b46cccf8892783eb
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-07-29 12:32:22 +0300

    Update vanilla unary request-response test

--- python-client/main_async_vanilla_unary.py
+++ python-client/main_async_vanilla_unary.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import queue
 import threading
 import time
 import uuid
@@ -33,25 +32,15 @@ class ResponseTracker:
 class ReportPortalClient:
 
     def __init__(self, url):
-        self.is_running = False
         self.url = url
-        self.item_start_tracker = ResponseTracker()
-        self.item_finish_tracker = ResponseTracker()
-        self.generator_tracker = queue.Queue()
 
     async def __aenter__(self):
-        self.is_running = True
         self.channel = await grpc.aio.insecure_channel(self.url).__aenter__()
         self.client = reportportal_pb2_grpc.ReportPortalReportingStub(
             self.channel)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        while self.item_start_tracker.size() > 0 and \
-                self.item_finish_tracker.size() > 0:
-            await asyncio.sleep(0.1)
-
-        self.is_running = False
         await self.channel.__aexit__(exc_type, exc_val, exc_tb)
         return self
 
@@ -66,17 +55,13 @@ class ReportPortalClient:
         logger.debug('Launch finished: ' + response.uuid)
 
     async def start_item(self, rq):
-        self.item_start_tracker.track(rq.uuid)
         logger.debug('Starting Item:' + rq.uuid)
         response = await self.client.StartTestItem(rq)
-        self.item_start_tracker.acknowledge(response.uuid)
         logger.debug('Item started:' + response.uuid)
 
     async def finish_item(self, rq):
-        self.item_finish_tracker.track(rq.uuid)
         logger.debug('Finishing Item:' + rq.uuid)
         response = await self.client.FinishTestItem(rq)
-        self.item_finish_tracker.acknowledge(response.uuid)
         logger.debug('Item finished:' + response.uuid)
 
 
